d06d53d0fe73c19ad5d003a84678c93c956b9c79
amangeldi <1998-amangeldi@mail.ru>
2020-06-08 14:25:12 +0600

    Updates

---
+++ precache-manifest.a47b73fc31204a8b24dc728d9db4a3f7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50277c1fd1b1de0e3618f0e250a11eaa",
+    "url": "/myresume/index.html"
+  },
+  {
+    "revision": "f32b1059e589c4a85ae4",
+    "url": "/myresume/static/css/main.e4893237.chunk.css"
+  },
+  {
+    "revision": "b2720604def75c12c906",
+    "url": "/myresume/static/js/2.ffb8fd9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myresume/static/js/2.ffb8fd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32b1059e589c4a85ae4",
+    "url": "/myresume/static/js/main.7faa3bec.chunk.js"
+  },
+  {
+    "revision": "91547e94bcbebe550470",
+    "url": "/myresume/static/js/runtime-main.17f9af87.js"
+  },
+  {
+    "revision": "7360fd4ec8e9c0a0311a0e4b3630c034",
+    "url": "/myresume/static/media/bem.7360fd4e.jpeg"
+  },
+  {
+    "revision": "5ee1fc815b3ff624fc5eeabe43d21c05",
+    "url": "/myresume/static/media/bootstrap.5ee1fc81.jpg"
+  },
+  {
+    "revision": "482fb139aa1a9050d5152b89c804c227",
+    "url": "/myresume/static/media/close-3.482fb139.png"
+  },
+  {
+    "revision": "a9be1b4865de2f075d20c33a29828e0d",
+    "url": "/myresume/static/media/code.a9be1b48.jpeg"
+  },
+  {
+    "revision": "236253489349d24050bb6154d4bac044",
+    "url": "/myresume/static/media/css.23625348.jpg"
+  },
+  {
+    "revision": "c621617ec8132189c8d25e1739462dc6",
+    "url": "/myresume/static/media/grid.c621617e.png"
+  },
+  {
+    "revision": "12f43aad4d911dee3c96db2c6eebc4ef",
+    "url": "/myresume/static/media/html.12f43aad.jpg"
+  },
+  {
+    "revision": "0ba9ae8fbfbb1478d99f3409ac64200a",
+    "url": "/myresume/static/media/javascript3.0ba9ae8f.jpg"
+  },
+  {
+    "revision": "cad3efbbad3decd1fea9475391ef9a0b",
+    "url": "/myresume/static/media/less.cad3efbb.jpg"
+  },
+  {
+    "revision": "bd13e7af42744e3dd662e20a369771c7",
+    "url": "/myresume/static/media/phone-icon-935.bd13e7af.png"
+  },
+  {
+    "revision": "61e3d5ce3923b1a74eab3434df097990",
+    "url": "/myresume/static/media/react.61e3d5ce.jpg"
+  },
+  {
+    "revision": "d2bdb81e9c68eb7d0c7aed5800308c3a",
+    "url": "/myresume/static/media/sass.d2bdb81e.jpg"
+  },
+  {
+    "revision": "2fc966979772f099740be233c0440a82",
+    "url": "/myresume/static/media/vscode.2fc96697.jpg"
+  }
+]);
